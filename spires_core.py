6522584e01d261759fb8f2b4016fa5cf8b12ee57
Ned Bair <nbair@eri.ucsb.edu>
2022-04-28 13:02:29 -0700

    tried pyomo but not happy

--- spires_core.py
+++ spires_core.py
@@ -8,59 +8,42 @@ Created on Wed Apr 20 15:59:38 2022
 
 import numpy as np
 import numpy.linalg as la
-from scipy.optimize import minimize
+import pyomo.environ as pyomo
 
 def speedyinvert(F,R,R0,solarZ,shade):    
         
-    mth='COBYLA'
-    op={'disp': True}
-    bnds=[[0,1],[0,1],[30,1200],[0,1000]]
-    x0=[0.5,0.05,250,10]
-    modelRefl=np.zeros(len(R))
+    model = pyomo.ConcreteModel()
+    model.x1=pyomo.Var(initialize=0.5, bounds=(0,1))
+    model.x2=pyomo.Var(initialize=0.05, bounds=(0,1))
+    model.x3=pyomo.Var(initialize=250, bounds=(30,1200))
+    model.x4=pyomo.Var(initialize=10, bounds=(0,1000))
+    model.modelRefl=np.zeros(len(R))
     
-    def SnowDiff(x,modelRefl):
-        
+    def SnowDiff(m):
+
         for i in range(0,len(R)):
-            pts=np.array([i+1,solarZ,x[3],x[2]])
-            modelRefl[i]= F(pts)
+            pts=np.array([i+1,solarZ,m.x4.value,m.x3.value])
+            m.modelRefl[i]= F(pts)
             
-        modelRefl=x[0]*modelRefl+x[1]*shade+(1-(x[0]-x[1]))*R0    
-        diffR=la.norm(R-modelRefl)
+        m.modelRefl=m.x1.value*m.modelRefl+m.x2.value*shade+(1-(m.x1.value-m.x2.value))*R0    
+        diffR=la.norm(R-m.modelRefl)
         return diffR
-            
-    #construct the bounds in the form of constraints
-    #inequality: constraint is =>0 vs. equality: constraint = 0
-    #COBLYA only supports inequality constraints and not bounds
-    #so bounds have to be set as inequality constraints
-    cons = []
-    for factor in range(len(bnds)):
-        lower, upper = bnds[factor]
-        l = {'type': 'ineq',
-             'fun': lambda x, lb=lower, i=factor: x[i] - lb}
-        u = {'type': 'ineq',
-             'fun': lambda x, ub=upper, i=factor: ub - x[i]}
-        cons.append(l)
-        cons.append(u)
     
-    #  (x[0]+x[1])-1 <= 0 <-> x[0]+x[1] <= 1
-    cons.append(
-        {"type": "ineq", "fun": lambda x: (x[0]+x[1])-1}
-        );
+    model.obj = pyomo.Objective(rule=SnowDiff)
+    model.c1 = pyomo.Constraint( expr = model.x1 + model.x2 <= 1)  
+    opt = pyomo.SolverFactory('glpk')
+    opt.solve(model)
+    modelRefl1=model.modelRefl
+    res1=[model.x1.value,model.x2.value,model.x3.value,model.x4.value]
     
-    res1 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
-                    constraints=cons,options=op)
-    modelRefl1=modelRefl
-    #run again w/ only snow and shade endmembers
-    #by adding new constraint
-    #1-(x[0]+x[1]) <= 0 <-> 1 <= x[0]+x[1] 
-    cons.append(
-        {"type": "ineq", "fun": lambda x: 1-(x[0]+x[1])}
-        )
-    res2 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
-                      constraints=cons,options=op)
-    modelRefl2=modelRefl
-    #if fsca is with 2 pct, use fsca & fshade only (equality) solution
-    if (abs(res1.x[0]-res2.x[0]) < 0.02):
+    model.c1.deactivate
+    model.c2 = pyomo.Constraint ( expr = model.x1 + model.x2 == 1)
+    opt.solve(model)
+    modelRefl2=model.modelRefl
+    res2=[model.x1.value,model.x2.value,model.x3.value,model.x4.value]
+    
+    #if less than 2 pct diff, use model 2 (fsca+fshade only)
+    if (abs(res1[0]-res2[0]) < 0.02):
         res=res2
         modelRefl=modelRefl2
     else:

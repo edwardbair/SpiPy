9e49195ee808675945cb4ea5f8548768511dc301
Ned Bair <nbair@eri.ucsb.edu>
2022-04-27 18:10:42 -0700

    chaned to inequalities and tried COBYLA, but results are still not good

--- spires_core.py
+++ spires_core.py
@@ -12,10 +12,10 @@ from scipy.optimize import minimize
 
 def speedyinvert(F,R,R0,solarZ,shade):    
         
-    mth='SLSQP'
-    op={'disp': True,'maxiter':1000,'ftol':1e-9 }
-    bnds=((0,1),(0,1),(30,1200),(0,1000))
-    x0=(0.5,0.05,250,10)
+    mth='COBYLA'
+    op={'disp': True}
+    bnds=[[0,1],[0,1],[30,1200],[0,1000]]
+    x0=[0.5,0.05,250,10]
     modelRefl=np.zeros(len(R))
     
     def SnowDiff(x,modelRefl):
@@ -28,24 +28,43 @@ def speedyinvert(F,R,R0,solarZ,shade):
         diffR=la.norm(R-modelRefl)
         return diffR
             
-    cons= [{"type": "ineq", "fun": lambda x: np.array([1-x[0]+x[1]])}]
-    res_ineq = minimize(SnowDiff,x0,args=modelRefl,method=mth,bounds=bnds,
-                        constraints=cons,options=op)
-    modelRefl_ineq=modelRefl
+    #construct the bounds in the form of constraints
+    #inequality: constraint is =>0 vs. equality: constraint = 0
+    #COBLYA only supports inequality constraints and not bounds
+    #so bounds have to be set as inequality constraints
+    cons = []
+    for factor in range(len(bnds)):
+        lower, upper = bnds[factor]
+        l = {'type': 'ineq',
+             'fun': lambda x, lb=lower, i=factor: x[i] - lb}
+        u = {'type': 'ineq',
+             'fun': lambda x, ub=upper, i=factor: ub - x[i]}
+        cons.append(l)
+        cons.append(u)
+    
+    #  (x[0]+x[1])-1 <= 0 <-> x[0]+x[1] <= 1
+    cons.append(
+        {"type": "ineq", "fun": lambda x: (x[0]+x[1])-1}
+        );
+    
+    res1 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
+                    constraints=cons,options=op)
+    modelRefl1=modelRefl
     #run again w/ only snow and shade endmembers
-    cons= [
-        {"type": "ineq", "fun": lambda x: np.array([1-x[0]+x[1]])},
-        {"type": "eq", "fun": lambda x: np.array([1-x[0]+x[1]])}
-        ]
-    res_eq = minimize(SnowDiff,x0,args=modelRefl,method=mth,bounds=bnds,
+    #by adding new constraint
+    #1-(x[0]+x[1]) <= 0 <-> 1 <= x[0]+x[1] 
+    cons.append(
+        {"type": "ineq", "fun": lambda x: 1-(x[0]+x[1])}
+        )
+    res2 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
                       constraints=cons,options=op)
-    modelRefl_eq=modelRefl
+    modelRefl2=modelRefl
     #if fsca is with 2 pct, use fsca & fshade only (equality) solution
-    if (abs(res_eq.x[0]-res_ineq.x[0]) < 0.02):
-        res=res_eq
-        modelRefl=modelRefl_eq
+    if (abs(res1.x[0]-res2.x[0]) < 0.02):
+        res=res2
+        modelRefl=modelRefl2
     else:
-        res=res_ineq
-        modelRefl=modelRefl_ineq
+        res=res1
+        modelRefl=modelRefl1
         
-    return res,modelRefl,res_ineq,res_eq
+    return res,modelRefl,res1,res2

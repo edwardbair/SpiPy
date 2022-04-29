468f8600de31db4fb69087e5a0425f63f12a52ed
Ned Bair <nbair@eri.ucsb.edu>
2022-04-28 17:29:15 -0700

    tried basin hopping to make sure Im not getting a local minimum. Also scaled x, 0-1. still getting poor fits

--- spires_core.py
+++ spires_core.py
@@ -8,22 +8,35 @@ Created on Wed Apr 20 15:59:38 2022
 
 import numpy as np
 import numpy.linalg as la
-from scipy.optimize import minimize
+from scipy.optimize import minimize#,basinhopping
 
 def speedyinvert(F,R,R0,solarZ,shade):    
-        
-    mth='COBYLA'
-    op={'disp': True}
-    bnds=[[0,1],[0,1],[30,1200],[0,1000]]
-    x0=[0.5,0.05,250,10]
-    modelRefl=np.zeros(len(R))
     
+    #sz=0.3
+    jxa='3-point'
+    tl=1e-9
+    mth='SLSQP'
+    op={'disp': True}
+    bnds=np.array([[0,1],[0,1],[30,1200],[0,1000]])
+
+    #x0=[0.5,0.05,250,10]
+    x0=np.array([0.5,0.05,0.188,0.01])
+    
+    modelRefl=np.zeros(len(R))
+
     def SnowDiff(x,modelRefl):
-        
+        #prevent out of bounds guesses
+        x[x>1]=1
+        x[x<0]=0
+        #re-scale from 0-1 to original values
+        for i in range(0,len(x)):
+            rng=bnds[i][1]-bnds[i][0]  
+            x[i]=x[i]*rng+bnds[i][0]
+    
         for i in range(0,len(R)):
             pts=np.array([i+1,solarZ,x[3],x[2]])
             modelRefl[i]= F(pts)
-            
+        
         modelRefl=x[0]*modelRefl+x[1]*shade+(1-(x[0]-x[1]))*R0    
         diffR=la.norm(R-modelRefl)
         return diffR
@@ -34,7 +47,7 @@ def speedyinvert(F,R,R0,solarZ,shade):
     #so bounds have to be set as inequality constraints
     cons = []
     for factor in range(len(bnds)):
-        lower, upper = bnds[factor]
+        lower, upper = [0,1]
         l = {'type': 'ineq',
              'fun': lambda x, lb=lower, i=factor: x[i] - lb}
         u = {'type': 'ineq',
@@ -48,7 +61,12 @@ def speedyinvert(F,R,R0,solarZ,shade):
         );
     
     res1 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
-                    constraints=cons,options=op)
+                      constraints=cons,options=op,tol=tl, jac=jxa)
+    # minimizer_kwargs = dict(args=modelRefl, method=mth, 
+    #                         options=op, constraints=cons)
+    # res1 = basinhopping(SnowDiff, x0, minimizer_kwargs=minimizer_kwargs,
+    #                     stepsize=sz)
+    
     modelRefl1=modelRefl
     #run again w/ only snow and shade endmembers
     #by adding new constraint
@@ -57,7 +75,10 @@ def speedyinvert(F,R,R0,solarZ,shade):
         {"type": "ineq", "fun": lambda x: 1-(x[0]+x[1])}
         )
     res2 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
-                      constraints=cons,options=op)
+                      constraints=cons,options=op,tol=tl, jac=jxa)
+    
+    # res2 = basinhopping(SnowDiff, x0, minimizer_kwargs=minimizer_kwargs,
+    #                     stepsize=sz)
     modelRefl2=modelRefl
     #if fsca is with 2 pct, use fsca & fshade only (equality) solution
     if (abs(res1.x[0]-res2.x[0]) < 0.02):

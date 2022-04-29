50b0bcded5966e6babbf92bd7d338fae0f545f3f
Ned Bair <nbair@eri.ucsb.edu>
2022-04-28 17:40:35 -0700

    merging

--- spires_core.py
+++ spires_core.py
@@ -8,28 +8,7 @@ Created on Wed Apr 20 15:59:38 2022
 
 import numpy as np
 import numpy.linalg as la
-<<<<<<< HEAD
-import pyomo.environ as pyomo
 
-def speedyinvert(F,R,R0,solarZ,shade):    
-        
-    model = pyomo.ConcreteModel()
-    model.x1=pyomo.Var(initialize=0.5, bounds=(0,1))
-    model.x2=pyomo.Var(initialize=0.05, bounds=(0,1))
-    model.x3=pyomo.Var(initialize=250, bounds=(30,1200))
-    model.x4=pyomo.Var(initialize=10, bounds=(0,1000))
-    model.modelRefl=np.zeros(len(R))
-    
-    def SnowDiff(m):
-
-        for i in range(0,len(R)):
-            pts=np.array([i+1,solarZ,m.x4.value,m.x3.value])
-            m.modelRefl[i]= F(pts)
-            
-        m.modelRefl=m.x1.value*m.modelRefl+m.x2.value*shade+(1-(m.x1.value-m.x2.value))*R0    
-        diffR=la.norm(R-m.modelRefl)
-        return diffR
-=======
 from scipy.optimize import minimize#,basinhopping
 
 def speedyinvert(F,R,R0,solarZ,shade):    
@@ -76,27 +55,9 @@ def speedyinvert(F,R,R0,solarZ,shade):
              'fun': lambda x, ub=upper, i=factor: ub - x[i]}
         cons.append(l)
         cons.append(u)
->>>>>>> tmp
-    
-    model.obj = pyomo.Objective(rule=SnowDiff)
-    model.c1 = pyomo.Constraint( expr = model.x1 + model.x2 <= 1)  
-    opt = pyomo.SolverFactory('glpk')
-    opt.solve(model)
-    modelRefl1=model.modelRefl
-    res1=[model.x1.value,model.x2.value,model.x3.value,model.x4.value]
-    
-    model.c1.deactivate
-    model.c2 = pyomo.Constraint ( expr = model.x1 + model.x2 == 1)
-    opt.solve(model)
-    modelRefl2=model.modelRefl
-    res2=[model.x1.value,model.x2.value,model.x3.value,model.x4.value]
-    
-<<<<<<< HEAD
-    #if less than 2 pct diff, use model 2 (fsca+fshade only)
-    if (abs(res1[0]-res2[0]) < 0.02):
-=======
+
     res1 = minimize(SnowDiff,x0,args=modelRefl,method=mth,
-                      constraints=cons,options=op,tol=tl, jac=jxa)
+                    constraints=cons,options=op,tol=tl, jac=jxa)
     # minimizer_kwargs = dict(args=modelRefl, method=mth, 
     #                         options=op, constraints=cons)
     # res1 = basinhopping(SnowDiff, x0, minimizer_kwargs=minimizer_kwargs,
@@ -117,7 +78,7 @@ def speedyinvert(F,R,R0,solarZ,shade):
     modelRefl2=modelRefl
     #if fsca is with 2 pct, use fsca & fshade only (equality) solution
     if (abs(res1.x[0]-res2.x[0]) < 0.02):
->>>>>>> tmp
+
         res=res2
         modelRefl=modelRefl2
     else:

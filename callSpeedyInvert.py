ad2a36d7cf6e8401fdebc618f21db645f05c43a1
Ned Bair <nbair@eri.ucsb.edu>
2022-05-01 11:47:58 -0700

    finally matching answers

--- callSpeedyInvert.py
+++ callSpeedyInvert.py
@@ -30,16 +30,23 @@ idx=wl.argsort(axis=0)
 #matlab solutions for: fractional snow cover(fsca), 
 #fractional shade(fshade),
 #grain size (um), and dust (ppm)
-msol=np.array([[0.8930,0.1070,367.8819,20.1914],
-              [0.4939,0.5061,83.5660,44.9953]])
+#listed as 4 variable solution for pixel 1, then 3 variable solution for pixel 1,
+#repeated for pixel 2
+msol=np.array([[0.8848,0.0485,430.2819,18.2311], 
+              [0.8929,0.1071,367.8753,20.1885],
+              [0.4957,0.1262,487.8204,55.4392], 
+              [0.4942,0.5058,83.3307,45.8701]])
 #matlab solutions for reflectance corresponding to above
-mrefl=np.array([[0.8143,0.6879,0.8194,0.8240,0.1865,0.0084,0.0040],
-               [0.4680,0.4354,0.4654,0.4684,0.2309,0.0339,0.0175]])
+mrefl=np.array([
+    [0.8173,0.6855,0.8166,0.8258,0.1848,0.0267,0.0230],
+    [0.8143,0.6879,0.8194,0.8240,0.1865,0.0084,0.0040],
+    [0.4828,0.4476,0.4524,0.4723,0.1814,0.1052,0.0706],
+    [0.4681,0.4355,0.4653,0.4683,0.2307,0.0336,0.0174]])
 #matlab RMSE (2 solutions x 2 days because the solver tries a mixed pixel
 #(fsca,fshade,fother)
 # and a fully snow covered pixel (fsca,fshade only), so 2 solutions for
 # 2 days
-mrmse=np.array([[0.0186,0.0326],[0.0136,0.1055]])
+mrmse=np.array([0.0186,0.0326,0.0136,0.1055])
 #solar zenith angle for both days
 solarZ=np.array([24.0,24.71])
 
@@ -47,7 +54,7 @@ solarZ=np.array([24.0,24.71])
 #the look up table was created doing radiative transfer Mie scattering calcs
 Ffile='LUT_MODIS.mat'
 #ideal shade endmember
-shade=0
+shade=np.zeros(len(R[0]))
 
 #load LUT
 if 'F' not in locals():
@@ -64,26 +71,42 @@ if 'F' not in locals():
     F=irp.RegularGridInterpolator(
         points=[d['X4'],d['X3'],d['X2'],d['X1']],
         values=d['X'])        
-    plt.close('all')
+
+
+
+plt.close('all')
 
 #run inversion and plot results for both pixels
-for i in range(0,len(R)):
+for i in range(0, 2):
     #run inversion
-    res=speedyinvert(F,R[i],R0[i],solarZ[i],shade)
-    
-    #plot results
-    plt.figure(i)
-    #R
-    plt.plot(wl[idx],R[i][idx],'k-', label='R')
-    #python solution
-    plt.plot(wl[idx],res[1][idx],'r-', label=
-             'python model:\n(RMSE=%0.2f,%0.2f)\nfsca=%0.2f\nfshade=%0.2f\ndust=%d ppm\nrg=%d ug' 
-             %(res[2].fun,res[3].fun,res[0].x[0],res[0].x[1],res[0].x[2],res[0].x[3]))
-    plt.plot(wl[idx],mrefl[i][idx],'g-', label=
-             'matlab model:\n(RMSE=%0.2f,%0.2f*):\nfsca=%0.2f\nfshade=%0.2f\ndust=%d ppm\nrg=%d ug' 
-             %(mrmse[i][0],mrmse[i][1],msol[i][0],msol[i][1],msol[i][3],msol[i][2]))
+    res = speedyinvert(F, R[i], R0[i], solarZ[i], shade)
+    plt.figure(figsize=(10,5))
+    #measured R
+    plt.plot(wl[idx], R[i][idx], 'k-', label='R')
+    #plot both 4 and 3 variable python solutions  
+    str=('4-variable','3-variable')
+    for j in range(0, 3, 2):        
+        #python solution
+        if j==0:
+            str='4-variable'
+        if j==2:
+            str='3-variable'
+        plt.plot(wl[idx], res[j+1][idx], 
+                 label= str+' python model\n'+ '(RMSE=%0.2f) fsca=%0.2f fshade=%0.2f rg=%d ug dust=%d ppm'
+             % (res[j].fun,res[j].x[0],res[j].x[1],res[j].x[2],res[j].x[3]))
+    #plot both 4 and 3 variable matlab solutions
+    for j in range(0,2):
+        if j==0:
+            str='4-variable'  
+        if j==1:
+            str='3-variable'    
+        idt=2*(i+1)+(j)-2
+        plt.plot(wl[idx], mrefl[idt][idx],
+            label= str+' matlab model\n'+ '(RMSE=%0.2f) fsca=%0.2f fshade=%0.2f rg=%d ug dust=%d ppm'
+            %(mrmse[idt],msol[idt][0],msol[idt][1],msol[idt][2],msol[idt][3]))
+
     plt.legend(loc="upper right")
-    plt.title(
-        'pixel %d, RMSE values for 4 and 3 parameter models listed,\nw/ stats for choice %d param model shown (matlab choice starred)' 
-              %(i,res[4]))
-    
+    plt.title('pixel %d' %(i+1))
+    plt.ylim(0,1)
+    plt.xlabel('wavelength, um')
+    plt.ylabel('reflectance')
